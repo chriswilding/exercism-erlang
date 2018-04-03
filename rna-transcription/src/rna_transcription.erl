f3790513a6475cd3632d66aa40dfe0b4644624e4
Chris Wilding <hello@chriswilding.co.uk>
2018-04-03 22:56:51 +0100

    Update RNA transcription

--- rna-transcription/src/rna_transcription.erl
+++ rna-transcription/src/rna_transcription.erl
@@ -6,15 +6,15 @@ complement($C) -> $G;
 complement($G) -> $C;
 complement($T) -> $A;
 complement($A) -> $U;
-complement(_) -> throw(error).
+complement(_) -> error.
 
-valid(Strand) ->
-    lists:all(fun(C) -> lists:member(C, [$C, $G, $T, $A]) end, Strand).
+invalid(Strand) ->
+    lists:any(fun(C) -> complement(C) == error end, Strand).
 
 to_rna(Strand) ->
-    case valid(Strand) of
-        true -> lists:map(fun complement/1, Strand);
-        _ -> error
+    case invalid(Strand) of
+        true -> error;
+        _    -> lists:map(fun complement/1, Strand)
     end.
 
 test_version() -> 2.
