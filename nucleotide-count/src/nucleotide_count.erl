b0b12b0d601d5fdfc366d3ad6960b706408977f7
Chris Wilding <hello@chriswilding.co.uk>
2018-08-16 20:17:43 +0100

    Update nucleotide count

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -2,32 +2,22 @@
 
 -export([count/2, nucleotide_counts/1, test_version/0]).
 
+-define(VALID_NUCLEOTIDES, ["A", "T", "C", "G"]).
+
 count(Dna, N) ->
-    Valid = valid(N),
-    count(Dna, N, Valid).
-
-count(Dna, N, Valid) when Valid ->
-    Char = hd(N),
-    Filtered = lists:filter(fun(Base) -> Base =:= Char end, Dna),
-    length(Filtered);
-count(_, _, _) ->
-    erlang:error("Invalid nucleotide").
-
-
-increment(V) ->
-    V + 1.
+    case lists:member(N, ?VALID_NUCLEOTIDES) of
+        true ->
+            length(lists:filter(fun(Base) -> [Base] == N end, Dna));
+        false ->
+            erlang:error("Invalid nucleotide")
+    end.
 
 nucleotide_counts(Dna) ->
-    Map = lists:foldl(fun(Base, Acc) -> maps:update_with(Base, fun increment/1, 1, Acc) end, #{}, Dna),
     [
-     {"A", maps:get($A, Map, 0)},
-     {"T", maps:get($T, Map, 0)},
-     {"C", maps:get($C, Map, 0)},
-     {"G", maps:get($G, Map, 0)}
+     {"A", count(Dna, "A")},
+     {"T", count(Dna, "T")},
+     {"C", count(Dna, "C")},
+     {"G", count(Dna, "G")}
     ].
-
-valid(Dna) ->
-    ValidBases = sets:from_list([$A, $T, $C, $G]),
-    lists:all(fun(Base) -> sets:is_element(Base, ValidBases) end, Dna).
 
 test_version() -> 1.
