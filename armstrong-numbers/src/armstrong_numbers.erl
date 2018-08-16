6798d19bdb4add1045a3486b7cd2ec25fe3ca631
Chris Wilding <hello@chriswilding.co.uk>
2018-08-16 17:44:17 +0100

    Add armstrong numbers

---
+++ armstrong-numbers/src/armstrong_numbers.erl
@@ -0,0 +1,19 @@
+-module(armstrong_numbers).
+
+-export([is_armstrong_number/1, test_version/0]).
+
+is_armstrong_number(Number) ->
+    NOfDigits = trunc(math:floor(math:log10(Number) + 1)),
+    Digits = digits(Number, NOfDigits),
+	Result = lists:map(fun(D) -> trunc(math:pow(D, NOfDigits)) end, Digits),
+    lists:sum(Result) =:= Number.
+
+digits(Number, NOfDigits) ->
+    Seq = lists:seq(1, NOfDigits),
+    lists:map(fun(N) -> nth_digit(Number, N) end, Seq).
+
+nth_digit(Number, N) ->
+    Dividend = Number rem trunc(math:pow(10, N)),
+    Dividend div trunc(math:pow(10, N - 1)).
+
+test_version() -> 1.
