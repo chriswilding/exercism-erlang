674f46d54a8011c4b654d75bad9d6d61969a041b
Chris Wilding <hello@chriswilding.co.uk>
2018-08-22 21:59:41 +0100

    Update nucleotide count

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -3,7 +3,14 @@
 -export([count/2, nucleotide_counts/1, test_version/0]).
 
 count(Dna, N) when N =:= "A"; N =:= "T"; N =:= "C"; N =:= "G" ->
-    length(lists:filter(fun(Base) -> [Base] == N end, Dna));
+    lists:foldl(
+      fun
+          (Base, Acc) when [Base] =:= N -> Acc + 1;
+          (_, Acc) -> Acc
+      end,
+      0,
+      Dna
+     );
 count(_, _) ->
     erlang:error("Invalid nucleotide").
 
@@ -11,10 +18,10 @@ nucleotide_counts(Dna) ->
     {A, T, C, G} =
     lists:foldl(
       fun
-          ($A, {AccA, AccT, AccC, AccG}) -> {AccA+1, AccT, AccC, AccG};
-          ($T, {AccA, AccT, AccC, AccG}) -> {AccA, AccT+1, AccC, AccG};
-          ($C, {AccA, AccT, AccC, AccG}) -> {AccA, AccT, AccC+1, AccG};
-          ($G, {AccA, AccT, AccC, AccG}) -> {AccA, AccT, AccC, AccG+1}
+          ($A, {AccA, AccT, AccC, AccG}) -> {AccA + 1, AccT, AccC, AccG};
+          ($T, {AccA, AccT, AccC, AccG}) -> {AccA, AccT + 1, AccC, AccG};
+          ($C, {AccA, AccT, AccC, AccG}) -> {AccA, AccT, AccC + 1, AccG};
+          ($G, {AccA, AccT, AccC, AccG}) -> {AccA, AccT, AccC, AccG + 1}
       end,
       {0, 0, 0, 0},
       Dna
