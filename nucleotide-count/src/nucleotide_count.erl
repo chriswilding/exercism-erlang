9e9cfbac8b81956f3edca0f3bb6aed12d2d29204
Chris Wilding <hello@chriswilding.co.uk>
2018-08-22 21:52:39 +0100

    Update nucleotide count

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -2,8 +2,6 @@
 
 -export([count/2, nucleotide_counts/1, test_version/0]).
 
--define(VALID_NUCLEOTIDES, ["A", "T", "C", "G"]).
-
 count(Dna, N) when N =:= "A"; N =:= "T"; N =:= "C"; N =:= "G" ->
     length(lists:filter(fun(Base) -> [Base] == N end, Dna));
 count(_, _) ->
