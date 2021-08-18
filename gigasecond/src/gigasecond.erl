0a143e66404f99840c14ec193a81b36c97cd8d5f
Chris Wilding <hello@chriswilding.co.uk>
2021-08-18 20:55:48 +0100

    Update gigasecond

--- gigasecond/src/gigasecond.erl
+++ gigasecond/src/gigasecond.erl
@@ -4,8 +4,8 @@
 
 -define(GIGASECOND, 1000000000).
 
-from({Date, Time}) ->
-    Seconds = calendar:datetime_to_gregorian_seconds({Date, Time}),
-    calendar:gregorian_seconds_to_datetime(Seconds + ?GIGASECOND);
-from(Date) ->
-    from({Date, {0, 0, 0}}).
+from({_, _, _} = Date) ->
+    from({Date, {0, 0, 0}});
+from(DateTime) ->
+    Seconds = calendar:datetime_to_gregorian_seconds(DateTime),
+    calendar:gregorian_seconds_to_datetime(Seconds + ?GIGASECOND).
